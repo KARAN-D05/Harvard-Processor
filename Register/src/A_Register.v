2c4147747123ae5b8480c1f3643fdb0dde0ef7f0
Karan Diwan <karandiwan46@gmail.com>
2026-06-23 20:02:57 +0530

    Register src

--- Register/src/A_Register.v
+++ Register/src/A_Register.v
@@ -2,25 +2,20 @@ module A_Register #(
     parameter WIDTH = 8 
 )(
     input [WIDTH-1:0] in,
-    input enableA,
     input loadA,
     input rst,
     input clk,
-    output [WIDTH-1:0] out
+    output reg [WIDTH-1:0] out
 );
-
-   reg [WIDTH-1:0] val;
 
   always@(posedge clk or posedge rst) begin
     
-    if (rst) begin
-        val <= 8'b0000000;
+    if(rst) begin
+        out <= 8'b0000000;
     end else if (loadA) begin
-        val <= in;
+        out <= in;
     end
 
   end
-
-  assign out = enableA ? val : {{(WIDTH){1'bz}}};
 
 endmodule
